ba1e62e17cd9a87a1fa49e30b4ef178124b1110d
YuxinLi88 <116744852+YuxinLi88@users.noreply.github.com>
2024-09-23 10:51:02 -0400

    Style fix

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -21,17 +21,15 @@ public class InLabByHandTranslator implements Translator {
      * @param country the country
      * @return list of language abbreviations which are available for this country
      */
+    public static final String CANADA = "can";
+
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (CANADA.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -53,22 +51,19 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        // TODO Checkstyle: String literal expressions should be on the left side of an equals comparison
-        if (!country.equals("can")) {
+        if (!country.equals(CANADA)) {
             return null;
         }
-        if (language.equals("de")) {
-            return "Kanada";
+        String words = null;
+        if ("de".equals(language)) {
+            words = "Kanada";
         }
-        else if (language.equals("en")) {
-            return "Canada";
+        else if ("en".equals(language)) {
+            words = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            words = "加拿大";
         }
-        else {
-            return null;
-        }
+        return words;
     }
 }
