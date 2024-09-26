172f28614db9975f57b1ab4b7202aa92e7d565ea
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-09-26 18:42:15 -0400

    JSONDemo completed

--- src/main/java/org/translation/JSONDemo.java
+++ src/main/java/org/translation/JSONDemo.java
@@ -34,7 +34,8 @@ public class JSONDemo {
      */
     public static String getKeyOneOfSecond(JSONArray jsonArray) {
         // TODO: Complete this method.
-        return "";
+        JSONObject secondobject = jsonArray.getJSONObject(1);
+        return secondobject.getString("key1");
     }
 
 }
